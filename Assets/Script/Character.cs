fbc8a88452236a1de28a27eab833f1d9df24f60c
seanm00n <deccj97@naver.com>
2022-05-15 20:52:01 +0900

    Initial commit
    create scripts

---
+++ Assets/Script/Character.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : GameController
+{
+    Vector3 location;//캐릭터 좌표
+    public int hp;
+    public int mp;
+    public int ap;
+    public int exp;
+
+    public Character(){
+
+    }
+    public void fixTurret()
+    {
+
+    }
+    
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Script/GameController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    Character character = new Character();
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
