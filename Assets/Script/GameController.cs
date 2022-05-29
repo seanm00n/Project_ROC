8d32ce15fe350e0b9057677ec4ff21ec83175ccf
Sean Moon <deccj97@naver.com>
2022-05-29 18:27:26 +0900

    add GameController

---
+++ Assets/Script/GameController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+    //몬스터 생성, 스테이지 구현
+}
